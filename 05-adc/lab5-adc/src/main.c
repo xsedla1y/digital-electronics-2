8491774e942df5ac2bb0a98627429abb8e357fda
Ondřej Sedlák <xsedla1y@vutbr.cz>
2022-10-25 18:43:08 +0200

    lab5_end

--- 05-adc/lab5-adc/src/main.c
+++ 05-adc/lab5-adc/src/main.c
@@ -41,18 +41,29 @@ int main(void)
     // Configure Analog-to-Digital Convertion unit
     // Select ADC voltage reference to "AVcc with external capacitor at AREF pin"
 
+    ADMUX = ADMUX | (1<<REFS0);
+
     // Select input channel ADC0 (voltage divider pin)
+
+    ADMUX = ADMUX & ~(1<<MUX0 | 1<<MUX1 | 1<<MUX2 | 1<<MUX3);
 
     // Enable ADC module
 
+    ADCSRA |= (1<<ADEN);
+
     // Enable conversion complete interrupt
+
+    ADCSRA |= (1<<ADIE);
 
     // Set clock prescaler to 128
 
+    ADCSRA |= (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
 
     // Configure 16-bit Timer/Counter1 to start ADC conversion
     // Set prescaler to 33 ms and enable overflow interrupt
 
+    TIM1_overflow_33ms();
+    TIM1_overflow_interrupt_enable();
 
     // Enables interrupts by setting the global interrupt mask
     sei();
@@ -74,9 +85,21 @@ int main(void)
  * Function: Timer/Counter1 overflow interrupt
  * Purpose:  Use single conversion mode and start conversion every 100 ms.
  **********************************************************************/
+
 ISR(TIMER1_OVF_vect)
 {
-    // Start ADC conversion
+    static uint8_t no_of_overflows = 0;
+
+    no_of_overflows++;
+    if (no_of_overflows >= 3)
+    {
+      // Do this every 6 x 16 ms = 100 ms
+      no_of_overflows = 0;
+      // Count tenth of seconds 0, 1, ..., 9, 0, 1, ...
+
+      // Start ADC conversion
+      ADCSRA |= (1<<ADSC);
+    }
 }
 
 /**********************************************************************
@@ -86,10 +109,45 @@ ISR(TIMER1_OVF_vect)
 ISR(ADC_vect)
 {
     uint16_t value;
+    uint16_t value2;
     char string[4];  // String for converted numbers by itoa()
 
     // Read converted value
     // Note that, register pair ADCH and ADCL can be read as a 16-bit value ADC
     value = ADC;
     // Convert "value" to "string" and display it
+    itoa(value, string, 2);
+    lcd_gotoxy(8, 0);
+    lcd_puts("    ");
+    lcd_gotoxy(8, 0);
+    lcd_puts(string);
+    itoa(value, string, 16);
+    lcd_gotoxy(13,0); 
+    lcd_puts("   ");
+    lcd_gotoxy(13,0); 
+    lcd_puts(string);
+
+    // ADC measured:
+    // none: 1023
+    // select: 640
+    // left: 410
+    // down: 257
+    // up: 100
+    // right: 0
+    itoa(value, value2, 10);
+    lcd_gotoxy(8, 1);
+    lcd_puts("      ");
+    lcd_gotoxy(8, 1);
+    if (value2 > 830)
+        lcd_puts("none");
+    else if (value2 > 520)
+        lcd_puts("Select");
+    else if (value2 > 330)
+        lcd_puts("Left");
+    else if (value2 > 180)
+        lcd_puts("Down");
+    else if (value2 > 50)
+        lcd_puts("Up");
+    else
+      lcd_puts("Down");
 }
