38f3f02fefcb209b0b52dfa251a8ae4a2c78a2dd
Ondřej Sedlák <99559571+xsedla1y@users.noreply.github.com>
2022-11-23 23:31:21 +0100

    project

--- Project1/src/main.c
+++ Project1/src/main.c
@@ -25,9 +25,23 @@
  * 
  **********************************************************************/
 
+// ENCODER
 #define OutputCLK PD2
 #define OutputDT PD3
 #define OutputSW PB2
+
+// JOYSTICK
+#define OutputSWJ PB3
+
+// LEDS
+#define LED_1 PC6
+#define LED_2 PC5
+#define LED_3 PC4
+#define LED_4 PC3
+#define LED_5 PC2
+#define LED_6 PB5
+#define LED_7 PB4
+
 
 /* Includes ----------------------------------------------------------*/
 #include <avr/io.h>         // AVR device-specific IO definitions
@@ -61,14 +75,44 @@ int main(void)
     // Initialize display
     lcd_init(LCD_DISP_ON);
     lcd_gotoxy(0, 0);
+    lcd_gotoxy(0, 0);
+    lcd_puts("Counter:0");
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Configure Analog-to-Digital Convertion unit
+    // Select ADC voltage reference to "AVcc with external capacitor at AREF pin"
+
+    ADMUX = ADMUX | (1<<REFS0);
+
+    // Select input channel ADC0 (voltage divider pin)
+
+    ADMUX = ADMUX & ~(1<<MUX0 | 1<<MUX1 | 1<<MUX2 | 1<<MUX3);
+
+    // Enable ADC module
+
+    ADCSRA |= (1<<ADEN);
+
+    // Enable conversion complete interrupt
+
+    ADCSRA |= (1<<ADIE);
+
+    // Set clock prescaler to 128
+
+    ADCSRA |= (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+
+    // Configure 16-bit Timer/Counter1 to start ADC conversion
+    // Set prescaler to 33 ms and enable overflow interrupt
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // Configuration of 8-bit Timer/Counter2 for Stopwatch update
     // Set the overflow prescaler to 16 ms and enable interrupt
+
     TIM1_overflow_33ms();
     TIM1_overflow_interrupt_enable();
-    TIM0_overflow_1ms();
-    TIM0_overflow_interrupt_enable();
+    TIM2_overflow_1ms();
+    TIM2_overflow_interrupt_enable();
 
     // Enables interrupts by setting the global interrupt mask
     sei();
@@ -90,7 +134,7 @@ int main(void)
  * Purpose:  Update the stopwatch on LCD screen every sixth overflow,
  *           ie approximately every 100 ms (6 x 16 ms = 100 ms).
  **********************************************************************/
-ISR(TIMER0_OVF_vect)
+ISR(TIMER2_OVF_vect)
 {
     char string[4];
     aState = GPIO_read(&PIND,OutputCLK);
@@ -133,6 +177,72 @@ button = GPIO_read(&PINB,OutputSW);
 
 ISR(TIMER1_OVF_vect)
 {
-    
+    static uint8_t no_of_overflows = 0;
 
+    no_of_overflows++;
+    if (no_of_overflows >= 3)
+    {
+      // Do this every 6 x 16 ms = 100 ms
+      no_of_overflows = 0;
+      // Count tenth of seconds 0, 1, ..., 9, 0, 1, ...
+
+      // Start ADC conversion
+      ADCSRA |= (1<<ADSC);
+    }
+}
+
+/**********************************************************************
+ * Function: ADC complete interrupt
+ * Purpose:  Display converted value on LCD screen.
+ **********************************************************************/
+ISR(ADC_vect)
+{
+    uint16_t value;
+    char string[4];  // String for converted numbers by itoa()
+
+    // Read converted value
+    // Note that, register pair ADCH and ADCL can be read as a 16-bit value ADC
+    value = ADC;
+    // Convert "value" to "string" and display it
+
+
+/*    itoa(value, string, 10);
+    lcd_gotoxy(8, 0);
+    lcd_puts("    ");
+    lcd_gotoxy(8, 0);
+    lcd_puts(string);
+    itoa(value, string, 16);
+    lcd_gotoxy(13,0); 
+    lcd_puts("   ");
+    lcd_gotoxy(13,0); 
+    lcd_puts(string);
+*/
+
+    // ADC measured:
+    // none: 1023
+    // select: 640
+    // left: 410
+    // down: 257
+    // up: 100
+    // right: 0
+
+    itoa(value, string, 10);
+    // Converting a numeric string
+    int x = atoi(string);
+
+    lcd_gotoxy(8, 1);
+    lcd_puts("      ");
+    lcd_gotoxy(8, 1);
+    if (x > 830)
+        lcd_puts("none");
+    else if (x > 520)
+        lcd_puts("Select");
+    else if (x > 330)
+        lcd_puts("Left");
+    else if (x > 180)
+        lcd_puts("Down");
+    else if (x > 50)
+        lcd_puts("Up");
+    else
+      lcd_puts("Right");
 }
